36418db5b3a548b341f6845e44cc138ba4078369
lei <2391923850@qq.com>
2020-03-02 20:58:04 +0800

    maven example init

---
+++ src/main/java/com/maven/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.maven.example;
+
+/**
+ * @author lei
+ * @desc
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello maven!");
+    }
+}
